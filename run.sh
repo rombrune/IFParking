4ae129a80a644cdee1d14b291e84ce8ca6d93fd4
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-02-27 12:34:17 +0100

    Added run script that checks for leaks

---
+++ run.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+dir="bin"
+target="parking"
+
+ps -fu `whoami` | grep -v grep | grep $target > $dir/ps.before
+ipcs > $dir/ipcs.before
+
+echo "---------- Run -------------"
+echo "Running $dir/$target..."
+$dir/$target
+
+ps -fu `whoami` | grep -v grep | grep $target > $dir/ps.after
+ipcs > $dir/ipcs.after
+
+echo "---------- Report ----------"
+psDiff=`diff $dir/ps.before $dir/ps.after`
+if [ -z "$psDiff" ]; then
+	echo "Process diff: OK"
+	rm -f $dir/ps.error
+else
+	echo "Process diff:\n$psDiff\n"
+	echo $psDiff > $dir/ps.error
+fi
+
+ipcsDiff=`diff $dir/ipcs.before $dir/ipcs.after`
+if [ -z "$ipcsDiff" ]; then
+	echo "IPCS diff: OK"
+	rm -f $dir/ipcs.error
+else
+	echo "IPCS diff:\n$ipcsDiff"
+	echo $ipcsDiff > $dir/ipcs.error
+fi
+echo
